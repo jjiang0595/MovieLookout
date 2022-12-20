3be99bdedf88a8a7e3284b0ac6dcc80bc5b230cd
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-20 16:25:34 -0500

    navbar

--- movie-lookout/components/layout/MainNavigation.js
+++ movie-lookout/components/layout/MainNavigation.js
@@ -13,11 +13,17 @@ function MainNavigation() {
                 </form>
                 <nav className={styles.userNav}>
                     <div className={styles.userNav__iconBox}>
-                        <span>Watch List</span>
                         <svg className={styles.userNav__icon}>
                             <use href="/sprite.svg#icon-bookmarks"></use>
                         </svg>
                         <span className={styles.userNav__notification}>7</span>
+                        <span>Watch List</span>
+                    </div>
+                    <div className={styles.userNav__iconBox}>
+                        <svg className={styles.userNav__icon}>
+                            <use href="/sprite.svg#icon-user"></use>
+                        </svg>
+                        <span>Guest</span>
                     </div>
                 </nav>
         </header>
