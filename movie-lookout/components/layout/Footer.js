3606228df99d13fdd80cd3ec39a079c5628ca4fd
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-20 16:11:11 -0500

    Footer

---
+++ movie-lookout/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>© 2022-2023 Movie Lookout</p>
+        </footer>
+    );
+}
+
+export default Footer;
