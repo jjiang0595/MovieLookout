8ded525175dafd1c6c15b0600d0a6f1f42ad83e8
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-20 20:39:39 -0500

    footer credit attribution

--- movie-lookout/components/layout/Footer.js
+++ movie-lookout/components/layout/Footer.js
@@ -3,6 +3,12 @@ import styles from './Footer.module.scss';
 function Footer() {
     return (
         <footer className={styles.footer}>
+            <a href="https://www.themoviedb.org/">
+                <img src="/tmdb.svg" width="100" height="100"/>
+            </a>
+            <span>
+                    <p>All film-related metadata used in Movie Lookout, including actor, director and studio names, synopses, release dates, trailers and poster art is supplied by <a href="https://www.themoviedb.org/" className={styles.footer__a}>The Movie Database (TMDb)</a>.</p>
+            </span>
             <p>© 2022-2023 Movie Lookout</p>
         </footer>
     );
