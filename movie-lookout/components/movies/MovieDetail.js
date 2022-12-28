12002e379f982b7289649432b69475951195de76
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-27 22:30:06 -0500

    adding movie detail

--- movie-lookout/components/movies/MovieDetail.js
+++ movie-lookout/components/movies/MovieDetail.js
@@ -2,10 +2,16 @@ import styles from './MovieDetail.module.scss';
 
 const MovieDetail = (props) => {
     return (
-        <div className={styles.movie} style={{backgroundImage: `url${props.movieData.backdrop}`}}>
-            <img className={styles.movie__image} src={`http://image.tmdb.org/t/p/w500/${props.movieData.image}`} alt={props.movieData.title}/>
-            <span className={styles.movie__title}>{props.movieData.title}</span>
-            <span className={styles.movie__overview}>{props.movieData.overview}</span>
+        <div className={styles.movie}
+             style={{backgroundImage: `url(http://image.tmdb.org/t/p/w500/${props.movieData.backdrop})`}}>
+            <div className={styles.movie__content}>
+                <img className={styles.movie__image} src={`http://image.tmdb.org/t/p/w500/${props.movieData.image}`}
+                     alt={props.movieData.title}/>
+                <div className={styles.movie__header}>
+                    <span className={styles.movie__header__title}>{props.movieData.title}</span>
+                    <span className={styles.movie__header__overview}>{props.movieData.overview}</span>
+                </div>
+            </div>
         </div>
     )
 }
