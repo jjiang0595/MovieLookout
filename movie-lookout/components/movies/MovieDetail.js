2a6b6b60f94c574e8db3fba3e4d96ab764a43331
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-29 23:02:21 -0500

    adding a favorites handler

--- movie-lookout/components/movies/MovieDetail.js
+++ movie-lookout/components/movies/MovieDetail.js
@@ -1,6 +1,11 @@
 import styles from './MovieDetail.module.scss';
 
 const MovieDetail = (props) => {
+
+    const addToListHandler = () => {
+        alert('Added to Watch List');
+    }
+
     return (
         <div className={styles.movie}
              style={{backgroundImage: `url(http://image.tmdb.org/t/p/w500/${props.movieData.backdrop})`}}>
@@ -9,6 +14,7 @@ const MovieDetail = (props) => {
                      alt={props.movieData.title}/>
                 <div className={styles.movie__header}>
                     <span className={styles.movie__header__title}>{props.movieData.title}</span>
+                    <button onClick={addToListHandler} className={styles.movie__header__watchlist}>Add to Watchlist</button>
                     <span className={styles.movie__header__overview}>{props.movieData.overview}</span>
                 </div>
             </div>
