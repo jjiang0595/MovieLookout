e36740a23c6661d05dec1b5c59b894dc26eaa579
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-29 23:02:06 -0500

    redirecting user to login if not logged in and clicked watchlist

--- movie-lookout/pages/watchlist/index.js
+++ movie-lookout/pages/watchlist/index.js
@@ -1,10 +1,22 @@
 import {useContext} from "react";
 import MovieWatchlist from "../../components/movies/MovieWatchlist";
+import AuthContext from "../../store/auth-context";
+import {router} from "next/client";
 
 const Watchlist = () => {
-    return (
-        <MovieWatchlist/>
-    );
+    const authCtx = useContext(AuthContext);
+    const isLoggedIn = authCtx.isLoggedIn;
+
+    if (isLoggedIn) {
+        return <MovieWatchlist />;
+    } else {
+        router.push({
+            pathname: '/login',
+            query: {
+                message: 'You must be logged in to view your watchlist.'
+            }
+        });
+    }
 }
 
 export default Watchlist;
